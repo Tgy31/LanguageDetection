e4991e52c2509fea3fc7bcd6475197db4d797b82
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-01-27 11:58:21 +0100

    OP + JSD

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*-coding:Latin-1 -*
 
 from NGram import NGram
+from NGramJSD import NGramJSD
 import datetime
 import sys, stat, os
 
@@ -20,7 +21,7 @@ testFiles=[filename for filename in testFiles if filename[0] != '.']
 print "Language samples : ", languageFiles
 print "Files to test : ", testFiles
 
-resultMatrix = []
+print "---------------- Vectors Way ------------------"
 
 for testFile in testFiles:
     print "\n\n--> ", testFile
@@ -30,7 +31,31 @@ for testFile in testFiles:
     for languageFile in languageFiles:
         languageText = open('Samples/'+languageFile).read()
         languageNGram = NGram(languageText, n)
-        print " ", languageFile, " : ", 1 - (languageNGram - testNGram)
+        print " ", languageFile, " : ", (languageNGram - testNGram)
+    
+    testEnd = datetime.datetime.now()
+    print "\n  Time : ", (testEnd - testStart)
+
+print "\n\n"
+print "----------- Jenson & Shannon Way --------------"
+
+for testFile in testFiles:
+    print "\n\n--> ", testFile
+    testText = open('Tests/'+testFile).read()
+    testStart = datetime.datetime.now()
+    testNGram = NGramJSD(testText, n)
+    total = 0
+    mini = 100000000
+    for languageFile in languageFiles:
+        languageText = open('Samples/'+languageFile).read()
+        languageNGram = NGramJSD(languageText, n)
+        diff = (languageNGram - testNGram)
+        total += diff
+        if diff < mini:
+            mini = diff
+            languageFound = languageFile
+        print " ", languageFile, " : ", diff
+    print "\n Result = ", languageFound, " with ", (1 - mini/total)*100, "%"
     
     testEnd = datetime.datetime.now()
     print "\n  Time : ", (testEnd - testStart)
